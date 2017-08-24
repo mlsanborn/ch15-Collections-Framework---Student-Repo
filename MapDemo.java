3f6ddc354f3fe0ef6e207a761c6e5f5ce724e48a
mlsanborn <mlsanborn@stu.naperville203.org>
2017-08-24 11:35:15 -0500

    bgbgy

---
+++ MapDemo.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+   This program demonstrates a map that maps names to colors.
+*/
+public class MapDemo
+{
+   public static void main(String[] args)
+   {
+	   //create Map: favoriteColors
+
+
+	  //create Set<String> keySet
+
+      // Print all keys and values in the map
+
+
+      for ()
+      {
+
+      }
+   }
+}
